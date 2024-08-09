3eacbbe43cc977ddb6e8ee5829d59e0a4f36b1da
92chougulesagar <sagar4gis@gmail.com>
2024-08-09 14:00:27 +0530

    updateing code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Info from './Components/Info';
+import Navbar from './Components/Navbar';
+import Form from './Components/Form';
 
 function App() {
 
   return (
     <div className="App">
-      <Info />
+      {/* <Info /> */}
+      <Navbar />
+      <Form />
     </div>
   );
 }
